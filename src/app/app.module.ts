01a1eeb28f7f493855de4715d48834bce3a8216a
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-24 13:55:22 +0100

    Added geometry component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MyPalComponent } from './my-pal/my-pal.component';
+import { GeometryComponent } from './geometry/geometry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyPalComponent
+    MyPalComponent,
+    GeometryComponent
   ],
   imports: [
     BrowserModule
