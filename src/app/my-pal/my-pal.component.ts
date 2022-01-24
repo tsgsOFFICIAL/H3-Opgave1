780944b4c014d4d87403fb0669ec97f8b09c5ada
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-24 12:43:22 +0100

    Event Binding

--- src/app/my-pal/my-pal.component.ts
+++ src/app/my-pal/my-pal.component.ts
@@ -10,6 +10,10 @@ export class MyPalComponent implements OnInit {
   description: string = "Gorillaer (Gorilla) er en slægt af store menneskeaber, og samtidigt de største af primaterne. De er planteædere, og bebor skovene i det centrale Afrika. Gorillaer er truet af udryddelse og er mål for krybskytteri. Hannerne bliver mellem 1,65 m og 1,75 m høje og vejer mellem 140 kg og 165 kg. Hunnerne vejer omkring det halve af hannerne. Joachim er en gorilla.";
   imgSrc: string = "assets/img/joachim.jpg";
   
+  ImageHover() {
+    alert(`Det her er Joachim`);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
