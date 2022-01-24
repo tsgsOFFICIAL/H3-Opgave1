71a496924c5deddb5b135ac7b7c621d96d6f1305
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-24 11:50:52 +0100

    Gorilla George

--- src/app/my-pal/my-pal.component.ts
+++ src/app/my-pal/my-pal.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-pal.component.css']
 })
 export class MyPalComponent implements OnInit {
-
+  name: string = "Joachim";
+  description: string = "Gorillaer (Gorilla) er en slægt af store menneskeaber, og samtidigt de største af primaterne. De er planteædere, og bebor skovene i det centrale Afrika. Gorillaer er truet af udryddelse og er mål for krybskytteri. Hannerne bliver mellem 1,65 m og 1,75 m høje og vejer mellem 140 kg og 165 kg. Hunnerne vejer omkring det halve af hannerne. Joachim er en gorilla.";
+  imgSrc: string = "assets/img/joachim.jpg";
+  
   constructor() { }
 
   ngOnInit(): void {
